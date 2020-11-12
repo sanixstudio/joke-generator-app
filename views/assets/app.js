655fadb04ce8936ee5e06d0bfd162cec21fe361e
awwmicky <michael_fred_alvarez@yahoo.com>
2020-11-11 20:53:24 -0800

    collab update

--- views/assets/app.js
+++ views/assets/app.js
@@ -1,7 +1,38 @@
-console.log('client testing')
+/* 
+TASKS 
+- Adnan - DOM reference
+- Adnan - css/html jokes
+- Micky - reveal btn add event listener
+- Herman- next btn add event listener
+- Herman - create fn to invoke fn 
+*/
 
-axios.get('./api/joke')
-.then(res => {
-    console.log(res.data)
+const prevBtn = document.querySelector('#prevBtn')
+const nextBtn = document.querySelector('#nextBtn')
+const revealBtn = document.querySelector('#submit')
+const displayedText = document.querySelector('#displayText')
+let punchlineTxt;
+let setupTxt;
+
+// invoke function and gets joke from API
+getJoke()
+
+function getJoke(){
+    axios.get('./api/joke')
+    .then(res => {
+        console.log(res.data)
+        punchlineTxt = res.data.jokePunchline
+        setupTxt = res.data.jokeSetup
+        displayedText.textContent = setupTxt
+    })
+    .catch(err => console.error(err))
+}
+
+// reveal joke
+revealBtn.addEventListener('click', () => {
+    displayedText.textContent = punchlineTxt;
 })
-.catch(err => console.error(err))
+
+nextBtn.addEventListener('click', () => {
+    getJoke()
+})
