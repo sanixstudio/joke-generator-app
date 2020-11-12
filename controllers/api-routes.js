e35de37caed7a73063cdc51efd6c0cff330739c8
Herman Liu <hermanliu95@gmail.com>
2020-11-11 16:43:40 -0800

    axios on jokeapi

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -1,5 +1,19 @@
 const router = require('express').Router();
+const axios = require('axios');
 
 router.get('/', (req,res) => { res.send('server testing') })
+
+// Test our apiRoute with jokeGenerator Api
+router.get('/joke', (req, res) => {
+    axios({
+        type: "GET",
+        url: 'https://official-joke-api.appspot.com/jokes/programming/random'
+    }).then( function(response) {
+        // console.log(response.data)
+        console.log(response.data[0].setup)
+        console.log(response.data[0].punchline)
+        res.send([response.data[0].setup , response.data[0].punchline ])
+    })
+})
 
 module.exports = router;
