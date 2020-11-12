e0ef683c9d689ac258218343d054696c570a153f
Herman Liu <hermanliu95@gmail.com>
2020-11-11 19:26:57 -0800

    changes in apiRoutes

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -1,19 +1,23 @@
 const router = require('express').Router();
 const axios = require('axios');
 
-router.get('/', (req,res) => { res.send('server testing') })
-
 // Test our apiRoute with jokeGenerator Api
 router.get('/joke', (req, res) => {
     axios({
         type: "GET",
         url: 'https://official-joke-api.appspot.com/jokes/programming/random'
-    }).then( function(response) {
+    })
+    .then( response => {
         // console.log(response.data)
         console.log(response.data[0].setup)
         console.log(response.data[0].punchline)
-        res.send([response.data[0].setup , response.data[0].punchline ])
+        // res.send([response.data[0].setup , response.data[0].punchline])
+        res.json({ 
+            'jokeSetup': response.data[0].setup ,
+            'jokePunchline': response.data[0].punchline
+        })
     })
+    .catch(err => console.error(err))
 })
 
 module.exports = router;
--- views/assets/app.js
+++ views/assets/app.js
@@ -1,5 +1,7 @@
 console.log('client testing')
 
 axios.get('./api/joke')
-.then(res => console.log(res.data))
+.then(res => {
+    console.log(res.data)
+})
 .catch(err => console.error(err))
