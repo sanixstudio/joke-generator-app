6acd76121eb71a13508b885f446a4cb777c0b59f
Adnan Niaz <sanixstudio@gmail.com>
2023-08-31 17:23:15 -0700

    enhanced UI and implemented proper user flow

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,55 +1,59 @@
-const prevBtn = document.querySelector('#prevBtn')
-const nextBtn = document.querySelector('#nextBtn')
-const revealBtn = document.querySelector('#submit')
-const displayedText = document.querySelector('#displayText')
+// DOM Elements
+const prevBtn = document.querySelector("#prevBtn");
+const nextBtn = document.querySelector("#nextBtn");
+const revealBtn = document.querySelector("#submit");
+const displayedText = document.querySelector("#displayText");
 let punchlineTxt;
 let setupTxt;
 
-//DB 
-let storedJokes = [];
+// Declare initial state
+let savedJokes = [];
 let count; // num
 
-// get joke from API
-getJoke()
-
-function getJoke () {    
-    prevBtn.classList.remove('displayNone') 
-    
-    axios.get('./api/joke')
-    .then(res => {
-        console.log(res.data)
-        storedJokes.push(res.data)
-        count = storedJokes.length - 1;
-        if (count === 0) {
-            prevBtn.classList.add('displayNone')
-        }
-        punchlineTxt = res.data.jokePunchline;
-        setupTxt = res.data.jokeSetup;
-        displayedText.textContent = setupTxt;
-    })
-    .catch(err => console.error(err))
-}
+// Get jokes
+fetchJoke();
 
 // reveal joke
-revealBtn.addEventListener('click', () => {
-    displayedText.textContent = punchlineTxt;
-})
+revealBtn.addEventListener("click", () => {
+  displayedText.textContent = punchlineTxt;
+  revealBtn.classList.add("displayNone");
+});
 
 // get new joke
-nextBtn.addEventListener('click', getJoke)
+nextBtn.addEventListener("click", fetchJoke);
 
-prevBtn.addEventListener('click', () => {
-    if (count === 0) return;
+prevBtn.addEventListener("click", () => {
+  if (count === 0) return;
 
-    count--;
+  count--;
 
-    if (count === 0) {
-        prevBtn.classList.add('displayNone')
-    }
+  if (count === 0) {
+    prevBtn.classList.add("displayNone");
+  }
 
-    punchlineTxt = storedJokes[count].jokePunchline;
-    setupTxt = storedJokes[count].jokeSetup;
-    displayedText.textContent = setupTxt;
+  punchlineTxt = savedJokes[count].jokePunchline;
+  setupTxt = savedJokes[count].jokeSetup;
+  displayedText.textContent = setupTxt;
+  revealBtn.classList.remove("displayNone");
+});
 
-    console.log(count, storedJokes[count])
-})
+// function to get jokes from API
+function fetchJoke() {
+  prevBtn.classList.remove("displayNone");
+
+  axios
+    .get("./api/joke")
+    .then((res) => {
+      console.log(res.data);
+      savedJokes.push(res.data);
+      count = savedJokes.length - 1;
+      if (count === 0) {
+        prevBtn.classList.add("displayNone");
+      }
+      punchlineTxt = res.data.jokePunchline;
+      setupTxt = res.data.jokeSetup;
+      displayedText.textContent = setupTxt;
+      revealBtn.classList.remove("displayNone");
+    })
+    .catch((err) => console.error(err));
+}
